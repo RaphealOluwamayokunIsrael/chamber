6dfcca24672030d5ab352b9f36dfade3f044afdf
Israel <raphealisrael5@gmail.com>
2026-09-19 18:05:32 +0100

    Connect My Chambers to welcome page

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,27 @@ import Features from "./components/Features";
 import ProductShowcase from "./components/ProductShowcase";
 import BuiltFor from "./components/BuiltFor";
 import Footer from "./components/Footer";
+import MyChambers from "./my-chambers/MyChambers";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+
       <Hero />
-      <Features />
-      <ProductShowcase />
-      <BuiltFor />
+
+      <main>
+        <section className="mx-auto w-full max-w-7xl px-6 py-16 sm:px-8 lg:px-10">
+          <MyChambers />
+        </section>
+
+        <Features />
+
+        <ProductShowcase />
+
+        <BuiltFor />
+      </main>
+
       <Footer />
     </>
   );
